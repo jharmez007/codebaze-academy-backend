d491516e077d10ab9fe0475bd785c84e72f222e4
Hackiod <geddyprime@gmail.com>
2025-09-23 13:58:58 +0100

    dd

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from .config import Config
 from .extensions import db, migrate, jwt
 from .routes import auth, student, admin, courses, enrollments, progress, comments
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/*": {"origins": "*"}})
     app.config.from_object(Config)
 
     db.init_app(app)
