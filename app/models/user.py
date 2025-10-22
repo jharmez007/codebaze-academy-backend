4ae037364c7e173b3ee1b61f221b9eeadd49bf6c
Hackiod <geddyprime@gmail.com>
2025-10-22 15:10:55 +0100

    ff

--- app/models/user.py
+++ app/models/user.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=True)
     role = db.Column(db.Enum('student', 'admin'), nullable=False, default='student')
     is_active = db.Column(db.Boolean, default=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
